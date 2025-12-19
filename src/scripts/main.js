5af8e659a3cded6b45eb47409c29eb24934ca4f6
mcalsing <marcelocalsing@gmail.com>
2025-12-19 15:00:53 -0300

    feat: implementado responsividade no slider e no nav do slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,19 @@
 // Inserção de entrelas com base na nota media do produto
 
-const averageRatingStars = document.querySelectorAll(".detail-notaMedia");
-
+const averageRatingStars = document.querySelectorAll(".detail-notaMedia")
+var count = 0
 averageRatingStars.forEach(el => {
-  const rating = parseFloat(el.textContent.trim());
+  const rating = parseFloat(el.textContent.trim())
+
+  count += 1;
+  //console.log(count)
 
   //console.log(rating)
   // Retira a nota do html
-  el.textContent = '';
+  el.textContent = ''
 
-  const starsContainer = document.createElement('div');
-  starsContainer.className = 'stars-container';
+  const starsContainer = document.createElement('div')
+  starsContainer.className = 'stars-container'
   
   // Ao colocar o cursor em cima das estrelas, mostra a nota media
   starsContainer.title = !isNaN(rating)
@@ -20,38 +23,38 @@ averageRatingStars.forEach(el => {
   if (!isNaN(rating)) {
     // Estrelas cheias
     for (let i = 0; i < Math.floor(rating); i++) {
-      const starImage = document.createElement('img');
-      starImage.src = '/src/assets/star-color2.png';
-      starImage.alt = 'rating star';
-      starImage.style.width = '15px';
+      const starImage = document.createElement('img')
+      starImage.src = '/src/assets/star-color2.png'
+      starImage.alt = 'rating star'
+      starImage.style.width = '15px'
 
-      starsContainer.appendChild(starImage);
+      starsContainer.appendChild(starImage)
     }
 
     // Estrelas pela metade
     if (rating % 1 >= 0.5) {
-      const starImage = document.createElement('img');
-      starImage.src = '/src/assets/star-half4.png'; 
-      starImage.alt = 'half star';
-      starImage.style.width = '15px';
+      const starImage = document.createElement('img')
+      starImage.src = '/src/assets/star-half4.png'
+      starImage.alt = 'half star'
+      starImage.style.width = '15px'
       
-      starsContainer.appendChild(starImage);
+      starsContainer.appendChild(starImage)
     }
        
   } else {
     // Caso não tenha nennhuma avaliação, 5 estrelas cinzas
     for (let i = 0; i < 5; i++) {
-      const starImage = document.createElement('img');
-      starImage.src = '/src/assets/star2.png';
-      starImage.alt = 'rating star';
-      starImage.style.width = '15px';
+      const starImage = document.createElement('img')
+      starImage.src = '/src/assets/star2.png'
+      starImage.alt = 'rating star'
+      starImage.style.width = '15px'
       
-      starsContainer.appendChild(starImage);
+      starsContainer.appendChild(starImage)
     }
   }
   
   // Insere o starsConteiner antes do .detail-notaMedia
-  el.parentNode.insertBefore(starsContainer, el);
+  el.parentNode.insertBefore(starsContainer, el)
 });
 
 
@@ -61,63 +64,86 @@ averageRatingStars.forEach(el => {
 const wishlist = document.querySelectorAll(".beon-showcase__wishlist-icon")
 
 wishlist.forEach(el => {
-  const hearthWishlist = document.createElement('img');
-  hearthWishlist.src = '/src/assets/heart-gray2.png';
+  const hearthWishlist = document.createElement('img')
+  hearthWishlist.src = '/src/assets/heart-gray2.png'
   hearthWishlist.style.width = '14px'
   hearthWishlist.alt = 'heart'
-  hearthWishlist.className = 'beon-showcase__wishlist-image';
+  hearthWishlist.className = 'beon-showcase__wishlist-image'
 
   // Evento para trocar por um coração colorido no hover
   hearthWishlist.addEventListener('mouseenter', () => {
-    hearthWishlist.src = '/src/assets/heart-color.png';
+    hearthWishlist.src = '/src/assets/heart-color.png'
     hearthWishlist.style.width = '15px'
     hearthWishlist.alt = 'heart'
-    hearthWishlist.className = 'beon-showcase__wishlist-image';
+    hearthWishlist.className = 'beon-showcase__wishlist-image'
   });
   
-//home/mcalsing/Documents/bn-teste-customizacao-componentes/src/assets/heart _gray2.png
 
   hearthWishlist.addEventListener('mouseleave', () => {
-    hearthWishlist.src = '/src/assets/heart-gray2.png';
+    hearthWishlist.src = '/src/assets/heart-gray2.png'
     hearthWishlist.style.width = '14px'
     hearthWishlist.alt = 'heart'
-    hearthWishlist.className = 'beon-showcase__wishlist-image';
+    hearthWishlist.className = 'beon-showcase__wishlist-image'
   });
 
-  el.appendChild(hearthWishlist);
+  el.appendChild(hearthWishlist)
 })
 
 
-// ----------------------- Angle slider ----------------------
+// ----------------------- Arrow slider ----------------------
 const buttonPrev = document.querySelector('.tns-controls button:first-child')
-const buttonNext = document.querySelector('.tns-controls button:last-child');
+const buttonNext = document.querySelector('.tns-controls button:last-child')
 
 // Prev
 const prevImage = document.createElement('img')
-prevImage.src = '/src/assets/arrow-left.png';
-prevImage.alt = 'prev button';
+prevImage.src = '/src/assets/arrow-left.png'
+prevImage.alt = 'prev button'
 prevImage.style.width = '20px'
-buttonPrev.innerHTML = '';
+buttonPrev.innerHTML = ''
 
 buttonPrev.appendChild(prevImage);
 
 // Next
-const nextImage = document.createElement('img');
-nextImage.src = '/src/assets/arrow-right.png';
-nextImage.alt = 'next button';
+const nextImage = document.createElement('img')
+nextImage.src = '/src/assets/arrow-right.png'
+nextImage.alt = 'next button'
 nextImage.style.width = '20px'
-buttonNext.innerHTML = '';
+buttonNext.innerHTML = ''
 
-buttonNext.appendChild(nextImage);
+buttonNext.appendChild(nextImage)
 
 
 // -------------------- Badge -----------------------
-const discount = document.querySelectorAll('.beon-showcase__item-discount');
+// Adiciona o sinal de menos na frente do número
+const discount = document.querySelectorAll('.beon-showcase__item-discount')
 
 discount.forEach(el => {
-  const value = parseFloat(el.textContent.trim());
+  const value = parseFloat(el.textContent.trim())
 
   if (!isNaN(value)) {
     el.innerHTML = `-${value}%`
   }
 })
+
+
+// -------------------- Slider nav control ----------------
+const sliderNavControl = document.querySelectorAll('.tns-nav button')
+
+let resizeTimeout
+
+// Função para descobrir o tamanho da tela e mudar o tamanho do slider nav control
+window.addEventListener('resize', () => {
+  clearTimeout(resizeTimeout)
+  resizeTimeout = setTimeout(() => {
+    console.log(window.innerWidth)
+    sliderNavControl.forEach(el => {
+      if (window.innerWidth >= 1280) el.style.width = '16.2%'
+      else if (window.innerWidth >= 1024) el.style.width = '13.9%'
+      else if (window.innerWidth >= 768) el.style.width = '9.6%'
+      else if (window.innerWidth >= 450) el.style.width = '6.6%'
+    })
+    // timeout de 50ms
+  }, 50);
+});
+
+
